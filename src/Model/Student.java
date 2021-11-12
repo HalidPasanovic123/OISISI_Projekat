f0f494d485fc62044bbebbc5de0462f8df0ab0a7
Halid <pasanovic.ra114.2019@uns.ac.rs>
2021-11-12 21:51:59 +0100

    Dodavanje nedostajucih atributa i izmena metoda u klasama Adresa,Profesor i Student i dodavanje glavnog prozora

--- src/Model/Student.java
+++ src/Model/Student.java
@@ -1,6 +1,7 @@
 package Model;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 
 public class Student {
 	
@@ -15,9 +16,12 @@ public class Student {
 	private int godinaStudija;
 	private Status status;
 	private float prosecnaOcena;
+	private ArrayList<OcenaNaIspitu> ocenePolozenihPredmeta;
+	private ArrayList<OcenaNaIspitu> nepolozeniPredmeti;
 	
 	public Student(String ime, String prezime, LocalDate datumRodjenja, String adresa, String kontakt, String eMail,
-			String indeks, LocalDate godinaUpisa, int godinaStudija, Status status, float prosecnaOcena) {
+			String indeks, LocalDate godinaUpisa, int godinaStudija, Status status, float prosecnaOcena,
+			ArrayList<OcenaNaIspitu> ocenePolozenihPredmeta, ArrayList<OcenaNaIspitu> nepolozeniPredmeti) {
 		super();
 		this.ime = ime;
 		this.prezime = prezime;
@@ -30,8 +34,10 @@ public class Student {
 		this.godinaStudija = godinaStudija;
 		this.status = status;
 		this.prosecnaOcena = prosecnaOcena;
+		this.ocenePolozenihPredmeta = ocenePolozenihPredmeta;
+		this.nepolozeniPredmeti = nepolozeniPredmeti;
 	}
-	
+
 	public Student() {
 		super();
 	}
@@ -39,89 +45,105 @@ public class Student {
 	public String getIme() {
 		return ime;
 	}
-	
+
 	public void setIme(String ime) {
 		this.ime = ime;
 	}
-	
+
 	public String getPrezime() {
 		return prezime;
 	}
-	
+
 	public void setPrezime(String prezime) {
 		this.prezime = prezime;
 	}
-	
+
 	public LocalDate getDatumRodjenja() {
 		return datumRodjenja;
 	}
-	
+
 	public void setDatumRodjenja(LocalDate datumRodjenja) {
 		this.datumRodjenja = datumRodjenja;
 	}
-	
+
 	public String getAdresa() {
 		return adresa;
 	}
-	
+
 	public void setAdresa(String adresa) {
 		this.adresa = adresa;
 	}
-	
+
 	public String getKontakt() {
 		return kontakt;
 	}
-	
+
 	public void setKontakt(String kontakt) {
 		this.kontakt = kontakt;
 	}
-	
+
 	public String geteMail() {
 		return eMail;
 	}
-	
+
 	public void seteMail(String eMail) {
 		this.eMail = eMail;
 	}
-	
+
 	public String getIndeks() {
 		return indeks;
 	}
-	
+
 	public void setIndeks(String indeks) {
 		this.indeks = indeks;
 	}
-	
+
 	public LocalDate getGodinaUpisa() {
 		return godinaUpisa;
 	}
-	
+
 	public void setGodinaUpisa(LocalDate godinaUpisa) {
 		this.godinaUpisa = godinaUpisa;
 	}
-	
+
 	public int getGodinaStudija() {
 		return godinaStudija;
 	}
-	
+
 	public void setGodinaStudija(int godinaStudija) {
 		this.godinaStudija = godinaStudija;
 	}
-	
+
 	public Status getStatus() {
 		return status;
 	}
-	
+
 	public void setStatus(Status status) {
 		this.status = status;
 	}
-	
+
 	public float getProsecnaOcena() {
 		return prosecnaOcena;
 	}
-	
+
 	public void setProsecnaOcena(float prosecnaOcena) {
 		this.prosecnaOcena = prosecnaOcena;
+	}
+
+	public ArrayList<OcenaNaIspitu> getOcenePolozenihPredmeta() {
+		return ocenePolozenihPredmeta;
+	}
+
+	public void setOcenePolozenihPredmeta(ArrayList<OcenaNaIspitu> ocenePolozenihPredmeta) {
+		this.ocenePolozenihPredmeta = ocenePolozenihPredmeta;
+	}
+
+	public ArrayList<OcenaNaIspitu> getNepolozeniPredmeti() {
+		return nepolozeniPredmeti;
+	}
+
+	public void setNepolozeniPredmeti(ArrayList<OcenaNaIspitu> nepolozeniPredmeti) {
+		this.nepolozeniPredmeti = nepolozeniPredmeti;
 	}
 
 	@Override
@@ -129,7 +151,7 @@ public class Student {
 		return "Student [ime=" + ime + ", prezime=" + prezime + ", datumRodjenja=" + datumRodjenja + ", adresa="
 				+ adresa + ", kontakt=" + kontakt + ", eMail=" + eMail + ", indeks=" + indeks + ", godinaUpisa="
 				+ godinaUpisa + ", godinaStudija=" + godinaStudija + ", status=" + status + ", prosecnaOcena="
-				+ prosecnaOcena + "]";
+				+ prosecnaOcena + ", ocenePolozenihPredmeta=" + ocenePolozenihPredmeta + ", nepolozeniPredmeti="
+				+ nepolozeniPredmeti + "]";
 	}
-	
 }
