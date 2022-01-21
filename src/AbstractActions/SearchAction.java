3b24008b36a972c0192676d5728c4a944866103e
SpasojevicAleksa <spasojevic.ra136.2019@uns.ac.rs>
2022-01-21 10:28:36 +0100

    Search Studenta

--- src/AbstractActions/SearchAction.java
+++ src/AbstractActions/SearchAction.java
@@ -28,14 +28,59 @@ public class SearchAction extends AbstractAction{
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (MainWindow.getInstance().getTabs().getJTabbedPane().getSelectedIndex() == 0) {
-            
+        if (MainWindow.getInstance().getTabs().getJTabbedPane().getSelectedIndex() == 0) 
+        {
+            StudentSearch();
         } else if(MainWindow.getInstance().getTabs().getJTabbedPane().getSelectedIndex() == 1){
             ProfesorSearch();
         } else if(MainWindow.getInstance().getTabs().getJTabbedPane().getSelectedIndex() == 2){
             PredmetSearch();
         }
 
+    }
+
+    private void StudentSearch() {
+        if(searchInput.getText().length() == 0){
+            MainWindow.getInstance().getTabs().getTableStudent().getSorter().setRowFilter(null);
+        }
+        else
+        {
+            final Pattern regexStrings = Pattern.compile("([a-zA-ZčČćĆžŽđĐšŠ]+[\\s]*)+");
+            final Matcher tempString = regexStrings.matcher(searchInput.getText());
+            ArrayList<String> strings = new ArrayList<String>();
+            
+            while ( tempString.find() ) {
+                strings.add(tempString.group().trim());
+            }
+        
+            if (strings.size() == 1) 
+            {
+                MainWindow.getInstance().getTabs().getTableStudent().getSorter().setRowFilter(RowFilter.regexFilter(strings.get(0), 2));
+            } else if(strings.size() == 2)
+            {
+                ArrayList<RowFilter<Object,Object>> filters = new ArrayList<RowFilter<Object,Object>>();
+                filters.add(RowFilter.regexFilter(strings.get(0), 2));
+                filters.add(RowFilter.regexFilter(strings.get(1), 1));
+                RowFilter<Object,Object> filter = RowFilter.andFilter(filters);
+                MainWindow.getInstance().getTabs().getTableStudent().getSorter().setRowFilter(filter);
+            }
+            else if(strings.size() == 3)
+            {
+                final Pattern regexIndex = Pattern.compile("[A-Za-z]{2}-([1-9][0-9]{0,2})?-?([0-9]{4})?");
+                final Matcher tempIndex = regexIndex.matcher(searchInput.getText());
+                ArrayList<String> strings2 = new ArrayList<String>();
+                while ( tempIndex.find() ) {
+                    strings2.add(tempIndex.group().trim());
+                }
+                ArrayList<RowFilter<Object,Object>> filters = new ArrayList<RowFilter<Object,Object>>();
+                filters.add(RowFilter.regexFilter(strings.get(0), 2));
+                filters.add(RowFilter.regexFilter(strings.get(1), 1));
+                filters.add(RowFilter.regexFilter(strings2.get(0), 0));
+    
+                RowFilter<Object,Object> filter = RowFilter.andFilter(filters);
+                MainWindow.getInstance().getTabs().getTableStudent().getSorter().setRowFilter(filter);
+            }
+        }
     }
 
     private void ProfesorSearch() {
